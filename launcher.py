729874305c857da52ad90599047a3749c888a374
LeraSidorova <valeralera22@icloud.com>
2022-08-22 21:17:36 +0300

    All about client

--- launcher.py
+++ launcher.py
@@ -1,22 +1,19 @@
-"""Лаунчер"""
-
 import subprocess
 
-PROCESSES = []
+process = []
 
 while True:
-    ACTION = input('Выберите действие: q - выход, '
-                   's - запустить сервер и клиенты, x - закрыть все окна: ')
-
-    if ACTION == 'q':
+    action = input('Выберите действие: q - выход , s - запустить сервер, k - запустить клиенты x - закрыть все окна:')
+    if action == 'q':
         break
-    elif ACTION == 's':
-        clients_count = int(input('Введите количество тестовых клиентов для запуска: '))
+    elif action == 's':
         # Запускаем сервер!
-        PROCESSES.append(subprocess.Popen('python server_script.py.py', creationflags=subprocess.CREATE_NEW_CONSOLE))
+        process.append(subprocess.Popen('python server_script.py.py', creationflags=subprocess.CREATE_NEW_CONSOLE))
+    elif action == 'k':
+        clients_count = int(input('Введите количество тестовых клиентов для запуска: '))
         # Запускаем клиентов:
         for i in range(clients_count):
-            PROCESSES.append(subprocess.Popen(f'python client.py -n test{i + 1}', creationflags=subprocess.CREATE_NEW_CONSOLE))
-    elif ACTION == 'x':
-        while PROCESSES:
-            PROCESSES.pop().kill()
+            process.append(subprocess.Popen(f'python client.py -n test{i + 1}', creationflags=subprocess.CREATE_NEW_CONSOLE))
+    elif action == 'x':
+        while process:
+            process.pop().kill()
